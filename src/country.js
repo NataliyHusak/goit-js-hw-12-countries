679c27662432cc83080db5a79ca96e2c811bb3fa
Nataliia Husak <nataliy.husak@gmail.com>
2019-12-08 23:27:22 +0200

    01
    02

--- src/country.js
+++ src/country.js
@@ -10,7 +10,7 @@ input.addEventListener(
   debounce(e => {
     fetchCountries(e.target.value)
       .then(data => {
-        if (data.length <= 10 && data.length >=2 ) {
+        if (data.length <= 10 && data.length >= 2) {
           PNotify.error({
             text: 'Too much matches found. Please enter a more specific query!',
             delay: 1000,
@@ -26,27 +26,19 @@ input.addEventListener(
             console.log('counrty', counrty);
             acc += `<h2 class="country__title">${counrty.name}</h2>
             <div class="country__wrapper">
-
             <div>
-          <p><span style="font-weight: bold">Capital: </span>${
-            counrty.capital
-          }</p>
-          <p><span style="font-weight: bold">Population: </span>${
+          <p class="counrty_descr"> Capital: ${counrty.capital}</p>
+          <p class="counrty_descr"> Population: ${
             counrty.population
           } citizens</p>
-          <p><span style="font-weight: bold">Languages: </span>
-
-
+          <p class="counrty_descr">Languages:
           <ul>
           ${counrty.languages.reduce((acc, cur) => {
-            return (acc += `<li>${cur.name}</li>`);
+            return (acc += `<li class="country_leng">${cur.name}</li>`);
           }, '')}
           </ul></p>
           </div>
-          <div><img src="${
-            counrty.flag
-          }" width="600" height="400" alt="Flag"></div>`;
-
+          <div><img class="flag" src="${counrty.flag}"  alt="Flag"></div>`;
             return (ul.innerHTML = acc);
           }, '');
         } else {
@@ -56,55 +48,3 @@ input.addEventListener(
       .catch(error => console.warn(error));
   }, 500),
 );
-
-// input.addEventListener(
-//   'input',
-//   debounce(e => {
-//     fetchCountries(e.target.value)
-//       .then(data => {
-//         if (data.length <= 10 && data.length !== 1) {
-//           PNotify.error({
-//             text: 'Too much matches found. Please enter a more specific query!',
-//             addClass: 'custom nonblock',
-//             delay: 2000,
-//             icon: true,
-//           });
-//           return data.reduce((acc, counrty) => {
-//             acc += `<li>${counrty.name}</li>`;
-//             return (ul.innerHTML = acc);
-//           }, '');
-//         }
-//         if (data.length === 1) {
-//           return data.reduce((acc, counrty) => {
-//             console.log('counrty', counrty);
-//             acc += `<h2 class="country__title">${counrty.name}</h2>
-//             <div class="country__wrapper">
-
-//             <div>
-//           <p><span style="font-weight: bold">Capital: </span>${
-//             counrty.capital
-//           }</p>
-//           <p><span style="font-weight: bold">Population: </span>${
-//             counrty.population
-//           } citizens</p>
-//           <p><span style="font-weight: bold">Languages: </span>
-
-//           <ul>
-//           ${counrty.languages.reduce((acc, cur) => {
-//             return (acc += `<li>${cur.name}</li>`);
-//           }, '')}
-//           </ul></p>
-//           </div>
-//           <div><img src="${
-//             counrty.flag
-//           }" width="600" height="400" alt="Flag"></div>`;
-
-//             return (ul.innerHTML = acc);
-//           }, '');
-//         } else {
-//           return '';
-//         }
-//       })
-//       .catch(err => console.log(err));
-//   }, 500),
-// );
